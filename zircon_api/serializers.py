9e166dd2ea94f1321b59d82f2badcaefc09b4ba3
sohail aslam <sohailaslam0707@gmail.com>
2020-10-05 14:47:04 -0400

    added serilazer

---
+++ zircon_api/serializers.py
@@ -0,0 +1,9 @@
+# from django.contrib.auth.models import User, Group
+# from rest_framework import serializers
+# from . import models
+
+
+# class PostSerializer(serializers.HyperlinkedModelSerializer):
+#     class Meta:
+#         model = models.BoastsRoastsModel
+#         fields = ['id','isboast','post_body','post_upvote' ,'post_downvote','date_created','last_update','s_key','total']
