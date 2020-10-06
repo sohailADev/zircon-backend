ed0f7fd20f49a72698dc15ed9a379785545f64bf
zac Gerber <Zgerber91@gmail.com>
2020-10-06 11:33:49 -0400

    added views fopr login and signup

--- InstaGramUser/views.py
+++ InstaGramUser/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect, reverse
+from django.contrib.auth import authenticate, login
+from InstaGramUser.models import InstaGramUser
+from InstaGramUser.forms import LoginForm, SignupForm
 
-# Create your views here.
+def login_view(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            user = authenticate(request, username=data.get('username'), password=data.get("password"))
+            if user:
+                login(request, user)
+                # return render(request, 'index.html', {"form": form})
+                return HttpResponseRedirect(request.GET.get('next', reverse("homepage")))
+
+    form = LoginForm()
+    return render(request, 'base.html', {"form": form})
+
+
+def signup_view(request):
+    if request.method == "POST":
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            new_user = MyUser.objects.create_user(
+                username=data.get("username"),
+                password=data.get("password"),
+                displayname=data.get("displayname")
+            )
+            login(request, new_user)
+            return HttpResponseRedirect(reverse('homepage'))
+    form = SignupForm()
+    return render(request, 'base.html', {"form": form})
