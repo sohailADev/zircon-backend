462dee8fce881a1c3f8c4275d295d60d8a201cf1
sohail aslam <33616867+sohailADev@users.noreply.github.com>
2020-10-06 13:56:57 -0400

    deleted extra code

--- notification_api/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class NotificationApiConfig(AppConfig):
-    name = 'notification_api'
--- notification_api/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from InstaGramUser.models import  InstaGramUser
-from zircon_api.models import Post
-
-
-class Notification(models.Model):
-    user_to_notify = models.ForeignKey(InstaGramUser, on_delete=models.CASCADE, related_name="user_to_notify")
-    post_to_be_notify = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="post_to_be_notify")
-    is_seen = models.BooleanField(default=False)
