4b690cb021692e4d893cd2e0f86bcbf40bffbdf6
Kevin Blount <kvbchamp@gmail.com>
2020-10-06 10:57:16 -0400

    create post model fields

--- zircon_api/models.py
+++ zircon_api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils.timezone import now
 
-# Kevin still working
+class Post(models.Model):
+    # title = models.TextField()
+    # caption = models.CharField()
+    # image = models.ImageField(upload_to='images/')
+    # likes = 
+    # location = models.URLField()
+    # created_by = models.ForeignKey(InstagramUser)
+    # create_date = models.DateTimeField(default=now, editable=False)
+    pass
+
