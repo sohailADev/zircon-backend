80ce555a0c6076f3c24f4548271bc286a55e6494
zac Gerber <Zgerber91@gmail.com>
2020-10-07 10:42:14 -0400

    added login and signup models and serializers

--- zircon_api/models.py
+++ zircon_api/models.py
@@ -30,5 +30,21 @@ class Notification(models.Model):
     is_seen = models.BooleanField(default=False)
 
     
-    
+class Login(models.Model):
+    username = models.CharField(max_length=240)
+    password = models.CharField(widget=forms.PasswordInput)
 
+
+class Signup(models.Model):
+    username = models.CharField(max_length=240)
+    password = models.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = models.InstaGramUser
+        fields = [
+            'email',
+            'phone',
+            'bio',
+            'gender',
+            'website',
+        ]
